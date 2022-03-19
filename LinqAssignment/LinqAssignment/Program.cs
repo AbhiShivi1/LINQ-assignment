de224e7f516692cf5d56ae3c2ece6b2bf8b1e1c9
Abhinav <abhinav@promactinfo.com>
2022-03-19 15:38:37 +0530

    LINQ assignment

---
+++ LinqAssignment/LinqAssignment/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+
+    IList<Employee> employeeList = new List<Employee>() {
+            new Employee(){ EmployeeID = 1, EmployeeFirstName = "Rajiv", EmployeeLastName = "Desai", Age = 49},
+            new Employee(){ EmployeeID = 2, EmployeeFirstName = "Karan", EmployeeLastName = "Patel", Age = 32},
+            new Employee(){ EmployeeID = 3, EmployeeFirstName = "Sujit", EmployeeLastName = "Dixit", Age = 28},
+            new Employee(){ EmployeeID = 4, EmployeeFirstName = "Mahendra", EmployeeLastName = "Suri", Age = 26},
+            new Employee(){ EmployeeID = 5, EmployeeFirstName = "Divya", EmployeeLastName = "Das", Age = 20},
+            new Employee(){ EmployeeID = 6, EmployeeFirstName = "Ridhi", EmployeeLastName = "Shah", Age = 60},
+            new Employee(){ EmployeeID = 7, EmployeeFirstName = "Dimple", EmployeeLastName = "Bhatt", Age = 53}
+        };
+    IList<Salary> salaryList = new List<Salary>() {
+            new Salary(){ EmployeeID = 1, Amount = 1000, Type = SalaryType.Monthly},
+            new Salary(){ EmployeeID = 1, Amount = 500, Type = SalaryType.Performance},
+            new Salary(){ EmployeeID = 1, Amount = 100, Type = SalaryType.Bonus},
+            new Salary(){ EmployeeID = 2, Amount = 3000, Type = SalaryType.Monthly},
+            new Salary(){ EmployeeID = 2, Amount = 1000, Type = SalaryType.Bonus},
+            new Salary(){ EmployeeID = 3, Amount = 1500, Type = SalaryType.Monthly},
+            new Salary(){ EmployeeID = 4, Amount = 2100, Type = SalaryType.Monthly},
+            new Salary(){ EmployeeID = 5, Amount = 2800, Type = SalaryType.Monthly},
+            new Salary(){ EmployeeID = 5, Amount = 600, Type = SalaryType.Performance},
+            new Salary(){ EmployeeID = 5, Amount = 500, Type = SalaryType.Bonus},
+            new Salary(){ EmployeeID = 6, Amount = 3000, Type = SalaryType.Monthly},
+            new Salary(){ EmployeeID = 6, Amount = 400, Type = SalaryType.Performance},
+            new Salary(){ EmployeeID = 7, Amount = 4700, Type = SalaryType.Monthly}
+        };
+
+
+
+
+
+    public static void Main()
+    {
+        Program program = new Program();
+
+        program.Task1();
+
+        program.Task2();
+
+        program.Task3();
+        Console.ReadLine();
+    }
+
+    public void Task1()
+    {
+
+        Console.WriteLine("TASK 1 : ");
+        Console.WriteLine();
+
+        var totalsalary = from salary in salaryList
+                        group salary by salary.EmployeeID
+                          into sal
+                        select new
+                        {
+                            EmployeeID = sal.Key,
+                            Sum = sal.Sum(s => s.Amount)
+                        };
+
+        var Q1 = from employee in employeeList
+                    join salary in totalsalary
+                      on employee.EmployeeID
+                      equals
+                      salary.EmployeeID
+                    orderby employee.EmployeeFirstName
+                    select new
+                    {
+                        e = employee,
+                        salary = salary.Sum,
+                    };
+
+
+        foreach (var item in Q1)
+        {
+
+         Console.WriteLine($" Name: {item.e.EmployeeFirstName} {item.e.EmployeeLastName},     Total Salary: {item.salary}  ");
+         
+        }
+
+    }
+
+    public void Task2()
+    {
+        Console.WriteLine();
+       Console.WriteLine(" TASK 2 : \n");
+
+
+        var monthlySalary = from salary in salaryList
+                 where salary.Type == SalaryType.Monthly
+                 select salary;
+
+        var Q2 = from salary in monthlySalary
+                     join employee in employeeList
+                            on salary.EmployeeID
+                            equals
+                            employee.EmployeeID
+                     orderby employee.Age descending
+                     select new
+                     {
+                         salary,
+                         employee
+                     };
+
+        foreach (var item in Q2.Skip(1).Take(1))
+        {
+          
+            Console.WriteLine($" EmployeeID: {item.employee.EmployeeID}, Name: {item.employee.EmployeeFirstName} {item.employee.EmployeeLastName}, Age: {item.employee.Age}, Salary: {item.salary.Amount}");
+            Console.WriteLine();
+            
+        }
+    }
+
+    public void Task3()
+    {
+
+        Console.WriteLine("\n TASK 3 : \n");
+
+
+        var Avg = from salary in this.salaryList
+                    group salary by salary.EmployeeID
+                    into sal
+                    select new
+                    {
+                        EmployeeID = sal.Key,
+                        amt = sal.Average(s => s.Amount)
+                    };
+
+        var ageG = (from employee in employeeList
+                      where employee.Age > 30
+                      select employee);
+
+        var Q3 = from employee in ageG
+                     join salary in Avg
+                       on employee.EmployeeID
+                       equals
+                       salary.EmployeeID
+                     orderby employee.EmployeeFirstName
+                     select new
+                     {
+                         emp = employee,
+                         avg = salary.amt
+                     };
+        
+
+        foreach (var item in Q3)
+        {
+            Console.WriteLine($" EmployeeID: {item.emp.EmployeeID},      Name: {item.emp.EmployeeFirstName} {item.emp.EmployeeLastName},      Age: {item.emp.Age},    Mean salary: {item.avg:N2}");
+
+        }
+    }
+
+}
+
+public enum SalaryType
+{
+    Monthly,
+    Performance,
+    Bonus
+}
+
+public class Employee
+{
+    public int EmployeeID { get; set; }
+    public string EmployeeFirstName { get; set; }
+    public string EmployeeLastName { get; set; }
+    public int Age { get; set; }
+}
+
+public class Salary
+{
+    public int EmployeeID { get; set; }
+    public int Amount { get; set; }
+    public SalaryType Type { get; set; }
+}
